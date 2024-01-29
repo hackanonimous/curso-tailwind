dabd92b46b018e606745383d409ecaea518db6e0
hackanonimous <esi.segurity@gmail.com>
2024-01-28 21:11:34 -0500

    feat: add initial files
    proyecto initialization and installation of tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+const App=()=>{
+	return(
+		<div className="underline">hola div</div>
+	)
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+import ReactDom from "react-dom/client"
+import "./main.css"
+import App from "./App"
+
+ReactDom.createRoot(document.getElementById("root")).render(
+		<App/>
+	)
